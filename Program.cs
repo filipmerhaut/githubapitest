54e1780e4637c79347220e7700a892d4ff8dfacb
filipmerhaut <filip.merhaut@gmail.com>
2024-08-20 01:16:15 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Octokit;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace githubapitest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Github API Test");
+
+            Console.WriteLine("Enter the name of the user for which you want to list their repositories:");
+            var username = Console.ReadLine();
+
+            var github = new GitHubClient(new ProductHeaderValue("githubapitest"));
+            var user = await github.User.Get(username);
+            var repos = await github.Repository.GetAllForUser(user.Login);
+
+            foreach (var repo in repos.OrderByDescending(x => x.StargazersCount))
+            {
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("Name: {0}", repo.Name);
+                Console.WriteLine("URL: {0}", repo.HtmlUrl);
+                Console.WriteLine("Stars: {0}", repo.StargazersCount);
+            }
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,12 @@
+# Project Name
+GithubAPITest
+
+# Description
+C# console application sample for GitHub API integration.
+
+# Instructions
+Include the package for Octokit.Net, which is the official client library for the GitHub API written in C#:
+dotnet add package Octokit
+
+# Source
+https://stateful.com/blog/github-api-list-repositories
